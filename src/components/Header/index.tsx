24f54b6ded2910de96bfe9c2e80257227284b797
zach <zachalvarezxvi@gmail.com>
2022-11-18 22:34:52 -0500

    Wallet address integrated with Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import  { Web3ModalContext } from '../../context/Web3ModalContext';
 
 const Header: React.FC = () => {
+
+  const { account, connect, disconnect } = React.useContext(Web3ModalContext);
 
   function ellipseAddress(
     address: string = "",
@@ -10,13 +13,22 @@ const Header: React.FC = () => {
     return `${address.slice(0, width + 2)}...${address.slice(-width)}`;
   }
 
+  const handleConnect = React.useCallback(() => {
+    connect();
+  }, [connect]);
+
+  const handleDisconnect = React.useCallback(() => {
+    disconnect();
+  }, [disconnect]);
+
   return (
     <nav className={styles.header}>
       <div className={styles.sides}>
         <div className={styles.left}>
         </div>
         <div className={styles.right}>
-          <div className={styles.connectButton}><span>NOT CONNECTED</span></div>
+          { !account ? ( <div className={styles.connectButton} onClick={handleConnect}><span>NOT CONNECTED</span></div>)
+          : (<div className={styles.connectButton} onClick={handleDisconnect}><span>{ellipseAddress(account)}</span></div>) }
         </div>
       </div>
     </nav>
